18adc1d002de2f23a24f4c99684de2ba0d894044
rifat <u1504002@student.cuet.ac.bd>
2022-11-17 19:04:57 +0600

    project4 is deleted

--- project4/enroll/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-
-class StudentRegistration(forms.Form):
-    name = forms.CharField(label_suffix='-')
-    email= forms.EmailField()
-    first_name= forms.CharField()
-    key= forms.CharField(widget=forms.HiddenInput())
--- project4/enroll/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    #path('', views.stinfo, name='stu-home'),
-    path('', views.formshow, name='register'),
-    
-]
